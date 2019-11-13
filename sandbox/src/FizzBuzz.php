3daca7684f72bd5dd520cf2a55718a16182f1da1
Ismail Koksal <ismail@koksal.fr>
2019-11-13 11:46:28 +0100

    Nouvelle implémentation de fizzbuzz et supression du dossier .idea

--- sandbox/src/FizzBuzz.php
+++ sandbox/src/FizzBuzz.php
@@ -6,55 +6,11 @@ namespace App;
 
 class FizzBuzz
 {
-    /**
-     * Retourne 'Fizz' pour les nombres
-     * qui sont des multiples de 3
-     *
-     * @param $i
-     * @return string
-     */
-    public function retourne_fizz_pour_les_multiples_de_3($i)
+    public function compte($i)
     {
-        if ($i % 3 == 0)
-            return 'Fizz';
-    }
-
-    /**
-     * Retourne 'Buzz' pour les nombres
-     * qui sont des multiples de 5
-     *
-     * @param $i
-     * @return string
-     */
-    public function retourne_fizz_pour_les_multiples_de_5($i)
-    {
-        if ($i % 5 == 0)
-            return 'Buzz';
-    }
-
-    /**
-     * Retourne 'FizzBuzz' pour les nombres
-     * qui sont des multiples de 3 et de 5
-     *
-     * @param $i
-     * @return string
-     */
-    public function retourne_fizz_pour_les_multiples_de_3_et_de_5($i)
-    {
-        if ($i % 3 == 0 && $i % 5 == 0)
-            return 'FizzBuzz';
-    }
-
-    /**
-     * Retourne le nombre pour les nombres
-     * qui ne sont ni multiples de 3 et ni multiples de 5
-     *
-     * @param $i
-     * @return string
-     */
-    public function retourne_nombre_si_pas_multiples_de_3_et_de_5($i)
-    {
-        if ($i % 3 != 0 && $i % 5 != 0)
-            return $i;
+        if ($i % 15 == 0) return 'FizzBuzz';
+        if ($i % 3 == 0) return 'Fizz';
+        if ($i % 5 == 0) return 'Buzz';
+        return $i;
     }
 }
--- sandbox/tests/FizzBuzzTest.php
+++ sandbox/tests/FizzBuzzTest.php
@@ -8,66 +8,42 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    /**
-     * Test pour s'assurer que les multiples de 3 retourne 'Fizz'.
-     */
-    public function test_multiple_de_3_retourne_fizz()
+    public function test_fizzbuzz_retourne_fizzbuzz()
     {
-        // On créer une partie de FizzBuzz
         $partie = new FizzBuzz();
-
-        // On test pour tous les multiples de 3 entre 0 et 100
-        for ($i = 0; $i <= 100; $i++) {
-            if ($i % 3 == 0) {
-                $this->assertSame('Fizz', $partie->retourne_fizz_pour_les_multiples_de_3($i));
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 15 == 0) {
+                $this->assertSame('FizzBuzz', $partie->compte($i));
             }
         }
     }
 
-    /**
-     * Test pour s'assurer que les multiples de 5 retourne 'Buzz'.
-     */
-    public function test_multiple_de_5_retourne_buzz()
+    public function test_fizzbuzz_retourne_fizz()
     {
-        // On créer une partie de FizzBuzz
         $partie = new FizzBuzz();
-
-        // On test pour tous les multiples de 5 entre 0 et 100
-        for ($i = 0; $i <= 100; $i++) {
-            if ($i % 5 == 0) {
-                $this->assertSame('Buzz', $partie->retourne_fizz_pour_les_multiples_de_5($i));
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 3 == 0 && $i % 15 != 0) {
+                $this->assertSame('Fizz', $partie->compte($i));
             }
         }
     }
 
-    /**
-     * Test pour s'assurer que les multiples de 3 et de 5 retourne 'FizzBuzz'.
-     */
-    public function test_multiple_de_15_retourne_fizzbuzz()
+    public function test_fizzbuzz_retourne_buzz()
     {
-        // On créer une partie de FizzBuzz
         $partie = new FizzBuzz();
-
-        // On test pour tous les multiples de 3 et de 5 entre 0 et 100
-        for ($i = 0; $i <= 100; $i++) {
-            if ($i % 3 == 0 && $i % 5 == 0) {
-                $this->assertSame('FizzBuzz', $partie->retourne_fizz_pour_les_multiples_de_3_et_de_5($i));
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 5 == 0 && $i % 15 != 0) {
+                $this->assertSame('Buzz', $partie->compte($i));
             }
         }
     }
 
-    /**
-     * Test pour s'assurer que les multiples de 3 et de 5 retourne 'FizzBuzz'.
-     */
-    public function test_nombre_pas_multiple_de_3_et_de_5_retourne_nombre()
+    public function test_fizzbuzz_retourne_nombre()
     {
-        // On créer une partie de FizzBuzz
         $partie = new FizzBuzz();
-
-        // On test pour tous les nombres qui ne sont ni multiples de 3 et ni multiples de de 5 entre 0 et 100
-        for ($i = 0; $i <= 100; $i++) {
-            if ($i % 3 != 0 && $i % 5 != 0) {
-                $this->assertSame($i, $partie->retourne_nombre_si_pas_multiples_de_3_et_de_5($i));
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 15 != 0 && $i % 3 != 0 && $i % 5 != 0) {
+                $this->assertSame($i, $partie->compte($i));
             }
         }
     }
