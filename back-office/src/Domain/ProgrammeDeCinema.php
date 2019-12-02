f90d6a4fc4581ef881db86efd6b087cc6ca7e109
Gerardo Aguilarcalderon <gerardo.aguilar_calderon@etudiant.univ-lr.fr>
2019-12-02 09:41:13 +0100

    US6 du responsable de la programmation

--- back-office/src/Domain/ProgrammeDeCinema.php
+++ back-office/src/Domain/ProgrammeDeCinema.php
@@ -3,4 +3,6 @@
 namespace App\Domain;
 interface ProgrammeDeCinema {
     public function getFilmsPourCinema():iterable;
+
+    public function mettreFilmAAffiche(Film $film, Cinema $cinema): bool;
 }
