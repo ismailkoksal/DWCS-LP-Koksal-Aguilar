12d8bd802644e1b43730b267a1e3f656b64b4076
Ismail Koksal <ismail@koksal.fr>
2019-12-06 14:19:34 +0100

    etape6

--- back-office/src/Domain/Cinema.php
+++ back-office/src/Domain/Cinema.php
@@ -4,9 +4,13 @@ namespace App\Domain;
 
 class Cinema {
     private $nom;
+    private $adresse;
+    private $thirdP;
 
-    public function __construct(string $nom) {
+    public function __construct(string $nom, string $adresse, string $thirdP) {
         $this->nom = $nom;
+        $this->adresse = $adresse;
+        $this->thirdP = $thirdP;
     }
 
     public function getNom(): string {
--- back-office/tests/Domain/CinemaTest.php
+++ back-office/tests/Domain/CinemaTest.php
@@ -8,7 +8,7 @@ use App\Domain\Cinema;
 class CinemaTest extends TestCase
 {
     public function test_un_cinema_expose_son_nom(){
-        $cinema=new Cinema("Le Lafayette");
+        $cinema=new Cinema("Le Lafayette", "adresse", "");
         $this->assertEquals("Le Lafayette",$cinema->getNom());
     }
 }
