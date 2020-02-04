3395ca5e08772199bc4d4e7dff3707c478880ece
Ismail Koksal <ismail@koksal.fr>
2020-02-04 10:40:43 +0100

    Remove all routes

--- api/src/Controller/ApiCinemaAdminController.php
+++ api/src/Controller/ApiCinemaAdminController.php
@@ -2,31 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Cinema;
 use App\Domain\Query\ListeCinemasHandler;
 use App\Domain\Query\ListeCinemasQuery;
-use App\Domain\Query\ProgrammationCinemaHandler;
-use App\Domain\Query\ProgrammationCinemaQuery;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiCinemaAdminController extends AbstractController
 {
-    /**
-     * @Route("/api/cinema/admin", name="api_cinema_admin")
-     */
-    public function index()
-    {
-        return $this->render('api_cinema_admin/index.html.twig', [
-            'controller_name' => 'ApiCinemaAdminController',
-        ]);
-    }
-
     /**
      * @Rest\View()
      * @Rest\Get("/api/cinemas", name="api_cinemas")
@@ -36,62 +18,5 @@ class ApiCinemaAdminController extends AbstractController
         $listeCinemas = $handler->handle($query);
 
         return $listeCinemas;
-    }
-
-    /**
-     * @Route("/api/cinemas/{cinema}", name="api_detail_cinema", methods={"GET"})
-     */
-    public function detailCinema(
-        Cinema $cinema,
-        ProgrammationCinemaHandler $programmationCinemaHandler,
-        SerializerInterface $serializer
-    ) {
-        $programmeQuery = new ProgrammationCinemaQuery($cinema);
-        $filmsAAffiche = $programmationCinemaHandler->handle($programmeQuery);
-
-        $filmsAAfficheJson = $serializer->serialize($filmsAAffiche, 'json');
-
-        return new JsonResponse($filmsAAfficheJson, 200, [], true);
-    }
-
-    /**
-     * @Route("/api/cinemas", name="api_add_cinema", methods={"POST"})
-     */
-    public function addCinema(Request $request, SerializerInterface $serializer) {
-        $data = $request->getContent();
-        $cinema = $serializer->deserialize($data, Cinema::class, 'json');
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($cinema);
-        $entityManager->flush();
-
-        return new JsonResponse('', Response::HTTP_CREATED, [], true);
-    }
-
-    /**
-     * @Route("/api/cinemas/{cinema}", name="api_update_cinema", methods={"PUT"})
-     */
-    public function updateCinema(
-        Cinema $cinema,
-        Request $request,
-        SerializerInterface $serializer
-    ) {
-        $data = $request->getContent();
-        $serializer->deserialize($data, Cinema::class, 'json', ['object_to_populate'=>$cinema]);
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($cinema);
-        $entityManager->flush();
-
-        return new JsonResponse('', Response::HTTP_OK, [], true);
-    }
-
-    /**
-     * @Route("/api/cinemas/{cinema}", name="api_delete_cinema", methods={"DELETE"})
-     */
-    public function deleteCinema(Cinema $cinema, Request $request) {
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($cinema);
-        $entityManager->flush();
-
-        return new JsonResponse('', Response::HTTP_OK, [], true);
     }
 }
